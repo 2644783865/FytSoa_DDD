e180145609f4e92a4619481f64bb40b13129aa1f
fuyu <715515390@qq.com>
2020-04-21 07:13:16 +0800

    更新数据，将主键改为varchar类型

--- DB/fyt_soa.sql
+++ DB/fyt_soa.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50719
  File Encoding         : 65001
 
- Date: 13/04/2020 09:22:10
+ Date: 21/04/2020 07:12:20
 */
 
 SET NAMES utf8mb4;
@@ -22,10 +22,10 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_admin`;
 CREATE TABLE `sys_admin`  (
-  `Id` bigint(25) NOT NULL COMMENT '唯一编号',
+  `Id` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '唯一编号',
   `RoleId` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '所属角色',
   `PostId` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '所属岗位',
-  `OrganizeId` bigint(25) NOT NULL COMMENT '所属部门',
+  `OrganizeId` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '所属部门',
   `OrganizeGroupId` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '所属上级部门组',
   `LoginAccount` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '登录账号',
   `LoginPassWord` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '登录密码',
@@ -52,9 +52,9 @@ CREATE TABLE `sys_admin`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_authority`;
 CREATE TABLE `sys_authority`  (
-  `RoleId` bigint(25) NOT NULL COMMENT '角色编号',
-  `AdminId` bigint(25) NULL DEFAULT NULL COMMENT '管理员编号',
-  `MenuId` bigint(25) NULL DEFAULT NULL COMMENT '菜单编号',
+  `RoleId` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '角色编号',
+  `AdminId` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '管理员编号',
+  `MenuId` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '菜单编号',
   `BtnFun` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '按钮功能组',
   `Types` int(11) NOT NULL DEFAULT 1 COMMENT '授权类型1=角色-菜单 2=用户-角色 3=角色-菜单-按钮功能'
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
@@ -64,8 +64,8 @@ CREATE TABLE `sys_authority`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_code`;
 CREATE TABLE `sys_code`  (
-  `Id` bigint(25) NOT NULL COMMENT '唯一编号',
-  `TypeGuid` bigint(25) NOT NULL COMMENT '分类编号',
+  `Id` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '唯一编号',
+  `TypeGuid` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '分类编号',
   `Name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '字典值名称',
   `CodeValues` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '字典值阈值',
   `Sort` int(11) NOT NULL DEFAULT 1 COMMENT '排序',
@@ -84,7 +84,7 @@ CREATE TABLE `sys_code`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_codetype`;
 CREATE TABLE `sys_codetype`  (
-  `Id` bigint(25) NOT NULL COMMENT '唯一编号',
+  `Id` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '唯一编号',
   `ParentGuid` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '父节点',
   `Layer` int(11) NOT NULL DEFAULT 1 COMMENT '层级',
   `Name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '分类名称',
@@ -103,7 +103,7 @@ CREATE TABLE `sys_codetype`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_log`;
 CREATE TABLE `sys_log`  (
-  `Id` bigint(25) NOT NULL,
+  `Id` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `LogType` int(11) NOT NULL COMMENT '日志类型  0=登录  1=操作',
   `Module` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '操作模块',
   `OperateType` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '操作类型:例如添加、修改',
@@ -122,9 +122,9 @@ CREATE TABLE `sys_log`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_menu`;
 CREATE TABLE `sys_menu`  (
-  `Id` bigint(25) NOT NULL COMMENT '唯一编号',
+  `Id` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '唯一编号',
   `Name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '菜单名称',
-  `ParentId` bigint(25) NULL DEFAULT NULL COMMENT '父节点',
+  `ParentId` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '父节点',
   `ParentGroupId` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '父节点集合组',
   `Code` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '权限标识',
   `Layer` int(11) NOT NULL DEFAULT 0 COMMENT '菜单层级',
@@ -147,8 +147,8 @@ CREATE TABLE `sys_menu`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_organize`;
 CREATE TABLE `sys_organize`  (
-  `Id` bigint(25) NOT NULL COMMENT '唯一编号',
-  `ParentId` bigint(25) NOT NULL COMMENT '父节点',
+  `Id` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '唯一编号',
+  `ParentId` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '父节点',
   `Name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '部门名称',
   `ParentIdList` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '父节点集合',
   `Layer` int(11) NOT NULL DEFAULT 0 COMMENT '部门层级',
@@ -168,16 +168,16 @@ CREATE TABLE `sys_organize`  (
 -- ----------------------------
 -- Records of sys_organize
 -- ----------------------------
-INSERT INTO `sys_organize` VALUES (1247889786656657408, 0, 'Fyt集团', '1247889786656657408', 1, 28, b'1', b'0', '1', '111', '1111', '2020-04-08 22:11:24', 'admin', NULL, NULL);
-INSERT INTO `sys_organize` VALUES (1248157435479330816, 1247889786656657408, '行政/人事', '1247889786656657408,1248157435479330816', 2, 1, b'1', b'0', '李四', '13688888888', '58555@qq.com', '2020-04-09 15:54:57', 'admin', NULL, NULL);
-INSERT INTO `sys_organize` VALUES (1248158071138684928, 1248157435479330816, '人事部', '1247889786656657408,1248157435479330816,1248158071138684928', 3, 1, b'1', b'0', '李总', '16544444444', '55545444@qq.com', '2020-04-09 15:57:28', 'admin', NULL, NULL);
+INSERT INTO `sys_organize` VALUES ('1247889786656657408', '0', 'Fyt集团', '1247889786656657408', 1, 28, b'1', b'0', '1', '111', '1111', '2020-04-08 22:11:24', 'admin', NULL, NULL);
+INSERT INTO `sys_organize` VALUES ('1248157435479330816', '1247889786656657408', '行政/人事', '1247889786656657408,1248157435479330816', 2, 1, b'1', b'0', '李四', '13688888888', '58555@qq.com', '2020-04-09 15:54:57', 'admin', NULL, NULL);
+INSERT INTO `sys_organize` VALUES ('1248158071138684928', '1248157435479330816', '人事部', '1247889786656657408,1248157435479330816,1248158071138684928', 3, 1, b'1', b'0', '李总', '16544444444', '55545444@qq.com', '2020-04-09 15:57:28', 'admin', NULL, NULL);
 
 -- ----------------------------
 -- Table structure for sys_post
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_post`;
 CREATE TABLE `sys_post`  (
-  `Id` bigint(25) NOT NULL COMMENT '唯一编号',
+  `Id` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '唯一编号',
   `Name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '岗位名称',
   `Number` varchar(6) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '岗位编码',
   `Sort` int(11) NOT NULL DEFAULT 1 COMMENT '排序',
@@ -192,15 +192,22 @@ CREATE TABLE `sys_post`  (
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
+-- Records of sys_post
+-- ----------------------------
+INSERT INTO `sys_post` VALUES ('1250983438480183296', '产品经理', '100000', 1, b'1', b'0', '产品经理', '2020-04-17 11:04:29', 'admin', NULL, NULL);
+INSERT INTO `sys_post` VALUES ('1251019168074043392', '项目经理', '100001', 1, b'1', b'0', '项目经理', '2020-04-17 13:26:27', 'admin', NULL, NULL);
+INSERT INTO `sys_post` VALUES ('1251019232876040192', '测试经理', '100002', 1, b'1', b'0', '测试经理', '2020-04-17 13:26:27', 'admin', NULL, NULL);
+
+-- ----------------------------
 -- Table structure for sys_role
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_role`;
 CREATE TABLE `sys_role`  (
-  `Id` bigint(25) NOT NULL COMMENT '唯一编号',
-  `Name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '角色名称',
-  `ParentId` bigint(25) NULL DEFAULT NULL COMMENT '角色父节点',
+  `Id` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '唯一编号',
+  `Name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '角色名称',
+  `ParentId` varchar(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '角色父节点',
   `Layer` int(11) NOT NULL DEFAULT 1 COMMENT '角色层级',
-  `Number` varchar(6) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '角色编号',
+  `Number` varchar(6) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '角色编号',
   `IsSystem` bit(1) NOT NULL DEFAULT b'0' COMMENT '是否超级管理员',
   `Status` bit(1) NOT NULL DEFAULT b'1' COMMENT '状态',
   `Sort` int(11) NOT NULL DEFAULT 1 COMMENT '排序',
@@ -212,5 +219,15 @@ CREATE TABLE `sys_role`  (
   `UpdateUser` char(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '修改人',
   PRIMARY KEY (`Id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_role
+-- ----------------------------
+INSERT INTO `sys_role` VALUES ('1251076856711614464', '默认', '0', 1, NULL, b'0', b'1', 1, NULL, b'0', '2020-04-17 17:15:41', 'admin', NULL, NULL);
+INSERT INTO `sys_role` VALUES ('1251122647241199616', '超级管理员', '1251076856711614464', 1, NULL, b'1', b'1', 1, NULL, b'0', '2020-04-17 20:17:38', 'admin', NULL, NULL);
+INSERT INTO `sys_role` VALUES ('1251126367240720384', '子管理员', '1251122647241199616', 1, NULL, b'0', b'1', 1, NULL, b'0', '2020-04-17 20:32:25', 'admin', NULL, NULL);
+INSERT INTO `sys_role` VALUES ('1251126415047397376', '职务', '0', 1, NULL, b'0', b'1', 1, NULL, b'0', '2020-04-17 20:32:37', 'admin', NULL, NULL);
+INSERT INTO `sys_role` VALUES ('1251126767943553024', '财务管理员', '1251126415047397376', 1, NULL, b'0', b'1', 1, NULL, b'0', '2020-04-17 20:34:01', 'admin', NULL, NULL);
+INSERT INTO `sys_role` VALUES ('1251126879725948928', '人事管理员', '1251126415047397376', 1, NULL, b'0', b'1', 1, NULL, b'0', '2020-04-17 20:34:28', 'admin', NULL, NULL);
 
 SET FOREIGN_KEY_CHECKS = 1;
